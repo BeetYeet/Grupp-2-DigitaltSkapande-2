2da92e7aad2a863ca03a25939d64e3e8a7d6d7e6
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2020-09-17 12:50:18 +0200

    Started with movement

---
+++ Assets/Player Input Actions.cs
@@ -0,0 +1,187 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Player Input Actions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Player Input Actions"",
+    ""maps"": [
+        {
+            ""name"": ""Main Action Map"",
+            ""id"": ""f58d8430-2ad3-454b-945a-92a6c252147e"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""3922d41a-b23d-4910-a13d-3be8d9a0ca14"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""891a7816-106b-4fbf-b252-46e28afc9330"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""e0635643-6c3d-4c78-a7a8-80689ecdc409"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""59a1fbaf-0326-4e67-b6c1-2a37d5907b4e"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""a37c839b-1da6-4189-ac41-c06498dd96af"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""cccb1cc6-ab7b-4ed1-aae8-a299210f0dc9"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""158c8528-2106-4120-923a-eb16f557667d"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Main Action Map
+        m_MainActionMap = asset.FindActionMap("Main Action Map", throwIfNotFound: true);
+        m_MainActionMap_Move = m_MainActionMap.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Main Action Map
+    private readonly InputActionMap m_MainActionMap;
+    private IMainActionMapActions m_MainActionMapActionsCallbackInterface;
+    private readonly InputAction m_MainActionMap_Move;
+    public struct MainActionMapActions
+    {
+        private @PlayerInputActions m_Wrapper;
+        public MainActionMapActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_MainActionMap_Move;
+        public InputActionMap Get() { return m_Wrapper.m_MainActionMap; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MainActionMapActions set) { return set.Get(); }
+        public void SetCallbacks(IMainActionMapActions instance)
+        {
+            if (m_Wrapper.m_MainActionMapActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_MainActionMapActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_MainActionMapActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_MainActionMapActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_MainActionMapActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public MainActionMapActions @MainActionMap => new MainActionMapActions(this);
+    public interface IMainActionMapActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform target;
+
+    private PlayerInputActions inputActions;
+    private Vector2 moveVector;
+
+    void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.MainActionMap.Move.performed += Move_performed;
+    }
+
+    private void Move_performed(InputAction.CallbackContext obj)
+    {
+        moveVector = obj.ReadValue<Vector2>();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(moveVector.x, 0f, moveVector.y) * .15f);
+        transform.LookAt(target, Vector3.up);
+    }
+}
