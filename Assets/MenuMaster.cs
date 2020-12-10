a5e6e6c5e1d27cf4a9a3e72aeb2a932b1281337c
zzWeeZz <zzWeeZznice@gmail.com>
2020-12-10 12:22:01 +0100

    no message

--- Assets/MenuMaster.cs
+++ Assets/MenuMaster.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.Audio;
 
 public class MenuMaster : MonoBehaviour
@@ -26,6 +27,7 @@ public class MenuMaster : MonoBehaviour
         audioSource.outputAudioMixerGroup = mixerGroupSFX;
         audioSource.PlayOneShot(SFX);
     }
+    
     public void enableOptionsMenu()
     {
         mainMenuCanvas.enabled = false;
---
+++ Assets/TextColorChange.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TextColorChange : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public Color[] colors;
+
+    public void ChangeColorOnText(int color)
+    {
+        text.color = colors[color];
+    }
+}
