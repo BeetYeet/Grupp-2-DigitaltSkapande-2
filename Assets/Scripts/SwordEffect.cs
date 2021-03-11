31ee8a644c56d78989944d0b5cc1a94a8e00beae
Alfred E SU18a <alfred.engstromer@elev.ga.lbs.se>
2021-03-11 12:32:58 +0100

    ny skit

---
+++ Assets/Scripts/SwordEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordEffect : MonoBehaviour
+{
+    public GameObject swordClashEffect;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // sword hit another sword (only hits its own layer)
+        Debug.Log("Swords Clashed");
+        if (swordClashEffect)
+        {
+            Instantiate(swordClashEffect, collision.GetContact(0).point, Quaternion.identity);
+        }
+    }
+}
