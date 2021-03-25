52af2a9de0ff275e05ecca39241a40361efa65a0
zzWeeZz <zzWeeZznice@gmail.com>
2021-03-25 11:19:14 +0100

    no message

--- Assets/SwordCompiler.cs
+++ Assets/SwordCompiler.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class SwordCompiler : MonoBehaviour
 {
-    public SwordCompiler[] blades, guards, handles;
+    public Swordcomponet[] blades, guards, handles;
     public int bladeId, guardId, handleId;
     [Space]
     public float damage, range, timeBetweenAttacks, health;
 
     private void Update()
     {
+           
+        
+    }
+
+    public void ReCalculate()
+    {
+        Debug.Log("Eqiuped blade " + blades[bladeId] + ", eqiuped guard " + guards[guardId] + ", eqiuped handle " + handles[handleId] + ".");
         damage = blades[bladeId].damage + guards[guardId].damage + handles[handleId].damage;
         range = blades[bladeId].range + guards[guardId].range + handles[handleId].range;
-        timeBetweenAttacks = blades[bladeId].damage + guards[guardId].damage + handles[handleId].damage;
-        damage = blades[bladeId].damage + guards[guardId].damage + handles[handleId].damage;
+        timeBetweenAttacks = blades[bladeId].basicAttackCooldown + guards[guardId].basicAttackCooldown + handles[handleId].basicAttackCooldown;
+        health = blades[bladeId].damage + guards[guardId].damage + handles[handleId].damage;
     }
 }
